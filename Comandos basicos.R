d03fcf9d1044478ff2171579064d5566f5f009fc
rodrigoheldt <33401739+rodrigoheldt@users.noreply.github.com>
2017-11-05 19:57:25 -0200

    Add files via upload

---
+++ Comandos basicos.R
@@ -0,0 +1,37 @@
+################################################################
+###                     Comandos basicos                     ###
+###                  Rodrigo Heldt Silveira                  ###
+###                  rodrigoheldt@gmail.com                  ###  
+################################################################
+
+
+#### 1. Comandos basicos ####
+
+
+#### 1.1. Operacoes ####
+
+2+5                             # Adicao
+sum(2,5,7)                      # Adicao utilizando a funcao 'sum'
+2-5                             # Subtracao
+2*5                             # Multiplicacao
+2/5                             # Divisao
+2^5                             # Potenciacao
+sqrt(16)                        # Raiz quadrada. usar a funcao 'sqrt'
+round(x = 3.456789, digits = 3) # Arredonda para 3 decimais
+round(x = 3.456789, digits = 2) # Arredonda para 2 decimais
+round(x = 3.456789, digits = 0) # Arredonda para 0 decimais
+
+
+#### 1.2. Criando e manipulando objetos ao longo do codigo ####
+
+x <- 2*5       # Atribui '1+2*5' ao objeto 'x'
+x              # Apresenta o objeto 'x'
+y <- (1+2)*x   # Agora o x, um objeto gravado no environment, pode ser utilizado em outras operacoes.
+y <- y*2       # Atualiza o valor do objeto 'y' por meio de uma operacoo que contem o proprio objeto 'y'
+
+
+
+
+
+
+
